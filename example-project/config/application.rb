5309ed5beb1cf060bcde2feb340cb01b7aa604fc
Petr Heinz <petr@betterstack.com>
2025-10-16 14:55:07 +0200

    Upgrade test project to Rails 8.1 RC1

--- example-project/config/application.rb
+++ example-project/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ExampleProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.1
 
     # Configuration for the application, engines, and railties goes here.
     #
